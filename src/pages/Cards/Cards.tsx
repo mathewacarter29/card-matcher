323e72cfbd8cc556f4ab6585e8e76b0958da60af
Mathew Carter <mathewacarter@gmail.com>
2025-04-18 22:02:53 -0400

    add timer and counter

--- src/pages/Cards/Cards.tsx
+++ src/pages/Cards/Cards.tsx
@@ -1,5 +1,6 @@
 import classes from "./Cards.module.css";
 import { useState } from "react";
+import { useStopwatch } from "react-timer-hook";
 
 interface CardProps {
   card: CardSettings;
@@ -37,6 +38,35 @@ const Card = (props: CardProps) => {
   );
 };
 
+type TimerSettings = {
+  minutes: number;
+  seconds: number;
+  hours: number;
+};
+
+interface TimerProps {
+  time: TimerSettings;
+  count: number;
+}
+
+const TimerCounter = (props: TimerProps) => {
+  const { time, count } = props;
+
+  return (
+    <div className={classes.timer} style={{display: "flex", justifyContent: 'space-around'}}>
+      <div>
+        <span>{count} turns taken</span>
+      </div>
+    <div>
+      <span>Time elapsed - </span>
+      <span>{String(time.hours).padStart(2, "0")}</span>:
+      <span>{String(time.minutes).padStart(2, "0")}</span>:
+      <span>{String(time.seconds).padStart(2, "0")}</span>
+    </div>
+    </div>
+  );
+};
+
 const Cards = () => {
   const NUM_CARDS = 20;
   const NUM_COLUMNS = 4;
@@ -54,38 +84,18 @@ const Cards = () => {
     "pink",
   ];
 
-  async function sleep(ms: number): Promise<void> {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-  }
-
-  const clickCard = async (i: number) => {
-    let newCards = cards.map((card, index) => {
-      return index == i ? { ...card, isRevealed: true } : card;
-    });
-    setCards(newCards);
-    let newSelected = [...selected, i];
-    // if this is the first card clicked, then just reveal it
-    if (newSelected.length >= 2) {
-      setLoading(true);
-      console.log("clicked second card");
-      // if colors dont match, set them both to be unrevealed
-      if (newCards[newSelected[0]].color !== newCards[newSelected[1]].color) {
-        // sleep for 1 second
-        await sleep(1000);
-        // make both selected cards unrevealed
-        newCards = cards.map((card, index) => {
-          return selected.includes(index)
-            ? { ...card, isRevealed: false }
-            : card;
-        });
-        setCards(newCards);
-      }
-      // unselect these cards
-      newSelected = [];
-    }
-    setSelected(newSelected);
-    setLoading(false);
-  };
+  const {
+    // totalSeconds,
+    // milliseconds,
+    seconds,
+    minutes,
+    hours,
+    // days,
+    // isRunning,
+    start,
+    pause,
+    reset,
+  } = useStopwatch();
 
   const initCards = () => {
     let init: CardSettings[] = [];
@@ -107,17 +117,85 @@ const Cards = () => {
   const [cards, setCards] = useState<CardSettings[]>(() => initCards());
   const [selected, setSelected] = useState<number[]>([]);
   const [loading, setLoading] = useState(false);
+  const [count, setCount] = useState(0);
+
+  async function sleep(ms: number): Promise<void> {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
+  const incrementCount = () => {
+    setCount((prevCount) => (prevCount += 1));
+  };
+
+  const clickCard = async (i: number) => {
+    if (count === 0) {
+      start();
+    }
+    let newCards = cards.map((card, index) => {
+      return index == i ? { ...card, isRevealed: true } : card;
+    });
+    setCards(newCards);
+    let newSelected = [...selected, i];
+    // if this is the first card clicked, then just reveal it
+    if (newSelected.length >= 2) {
+      incrementCount();
+      setLoading(true);
+      console.log("clicked second card");
+      // if colors dont match, set them both to be unrevealed
+      if (newCards[newSelected[0]].color !== newCards[newSelected[1]].color) {
+        // sleep for 1 second
+        await sleep(1000);
+        // make both selected cards unrevealed
+        newCards = cards.map((card, index) => {
+          return selected.includes(index)
+            ? { ...card, isRevealed: false }
+            : card;
+        });
+        setCards(newCards);
+      } else if (newCards.every((card) => card.isRevealed)) { // cards are a match - check if they are all revealed
+        // all cards are revealed, we have a winner!
+        pause();
+      }
+      // unselect these cards
+      newSelected = [];
+    }
+    setSelected(newSelected);
+    setLoading(false);
+  };
+
+  const onGoBack = () => {
+    setLoading(true);
+    setCards(initCards());
+    setSelected([]);
+    reset();
+    setCount(0);
+    setLoading(false);
+  };
 
   return (
-    <div
-      className={classes.container}
-      style={{ gridTemplateColumns: "auto ".repeat(NUM_COLUMNS), pointerEvents: loading ? "none" : "auto"}}
-    >
-      {cards.map((cardProp) => {
-        return (
-          <Card key={cardProp.index} card={cardProp} onClick={clickCard} />
-        );
-      })}
+    <div className={classes.container}>
+      <TimerCounter
+        time={{
+          hours,
+          minutes,
+          seconds,
+        }}
+        count={count}
+      />
+      <div
+        className={classes.board}
+        style={{
+          gridTemplateColumns: "auto ".repeat(NUM_COLUMNS),
+          pointerEvents: loading ? "none" : "auto",
+        }}
+      >
+        {cards.map((cardProp) => {
+          return (
+            <Card key={cardProp.index} card={cardProp} onClick={clickCard} />
+          );
+        })}
+      </div>
+      <button onClick={() => onGoBack()}>Go back</button>
     </div>
   );
 };
